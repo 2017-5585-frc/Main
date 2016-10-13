f1b3ce478e93e168b0b0e3e88991a41bfa275343
Ian Bolin <ibolin@myonc.org>
2016-10-13 08:54:07 -0400

    Revert "reversion"

--- 2016 main code/src/org/usfirst/frc/team5585/robot/OI.java
+++ 2016 main code/src/org/usfirst/frc/team5585/robot/OI.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team5585.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team5585.robot.commands.ExampleCommand;
-import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -13,9 +12,7 @@ public class OI {
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
-    Joystick xbox = new Joystick(1);
-    
-    Joystick joystick = new Joystick(0);
+    // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
     
     // There are a few additional built in buttons you can use. Additionally,
