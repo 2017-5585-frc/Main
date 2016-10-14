aa7671d6b9539ff0181bc43b7c502a49ccc54cd1
Ian Bolin <ibolin@myonc.org>
2016-10-14 08:40:26 -0400

    further refined shooter subsystem code, should be complete.

--- 2016 main code/src/org/usfirst/frc/team5585/robot/subsystems/Shooter.java
+++ 2016 main code/src/org/usfirst/frc/team5585/robot/subsystems/Shooter.java
@@ -14,20 +14,30 @@ public class Shooter extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	public void start() {
+		/**
+		 * start() takes no arguments and starts the shooter motors.
+		 */
 		upMotor.set(1.0);
 		lowMotor.set(1.0);
 	}
 	
 	public void stop() {
+		/**
+		 * stop() takes no arguments and stops the shooter motors.
+		 */
 		upMotor.stopMotor();
 		lowMotor.stopMotor();
 	}
-	public void aim(double amount) {
+	public void aim(int amount) {
+		/**
+		 * aim() controls the elevation of the shooter subsystem using the motor speeds. It takes an integer argument, which scales the degree of aiming.
+		 * An argument of 0 resets the shooter to initial state.
+		 */
 		double upSpeed = upMotor.getSpeed();
 		double lowSpeed = lowMotor.getSpeed();
 		if (amount > 0) {
-			lowSpeed += 0.1;
-			upSpeed -= 0.1;
+			lowSpeed += 0.1*amount;
+			upSpeed -= 0.1*amount;
 			if (lowSpeed > 1.0) {
 				lowSpeed = 1.0;
 			}
@@ -35,12 +45,26 @@ public class Shooter extends Subsystem {
 				upSpeed = 0.0;
 			}
 		}
+		if (amount < 0) {
+			lowSpeed -= 0.1*amount;
+			upSpeed +=0.1*amount;
+			if (lowSpeed < 0.0) {
+				lowSpeed = 0.0;
+			}
+			if (upSpeed > 1.0) {
+				upSpeed = 1.0;
+			}
+		}
+		if (amount == 0) {
+			lowSpeed = 1.0;
+			upSpeed = 1.0;
+		}
 		
 	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new start());
+        //TODO setDefaultCommand(new startShooter());
     }
 }
 
